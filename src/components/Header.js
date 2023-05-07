fc1636e03634a1dd813f38d34e4a5e0451b623ca
rithikrajyt <rrpandey855@gmail.com>
2023-05-07 18:47:33 +0530

    updated header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,6 @@
+import React from "react";
+import img1  from "../images/hero.jpg"
+
 export const Header = () => {
   return (
     <div className="relative px-4 pt-16 mx-auto lg:py-32 md:px-8 xl:px-20 sm:max-w-xl md:max-w-full">
@@ -6,15 +9,13 @@ export const Header = () => {
           <div className="max-w-xl mb-6">
             <div>
               <p className="inline-block px-3 py-px mb-4 text-xs font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
-                Brand new
+                Coming Soon!!!
               </p>
             </div>
-            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
-              The quick, brown fox
-              <br className="hidden md:block" />
-              jumps over{' '}
-              <span className="inline-block text-deep-purple-accent-400">
-                a lazy dog
+            <h2 className="max-w-lg mb-6 font-sans text-15xl font-bold tracking-tight text-gray-900 sm:text-4xl sm:leading-none">
+              Hire interns in 
+              <span className="inline-block text-deep-purple-accent-400 ml-2">
+                24 Hours.
               </span>
             </h2>
             <p className="text-base text-gray-700 md:text-lg">
@@ -43,8 +44,8 @@ export const Header = () => {
       </div>
       <div className="flex justify-center h-full overflow-hidden lg:w-2/3 xl:w-1/2 lg:absolute lg:justify-start lg:bottom-0 lg:right-0 lg:items-end">
         <img
-          src="https://kitwind.io/assets/kometa/full-browser.png"
-          className="object-cover object-top w-full h-64 max-w-xl -mb-16 rounded shadow-2xl lg:ml-64 xl:ml-8 lg:-mb-24 xl:-mb-28 lg:h-auto lg:max-w-screen-md"
+          src={img1}
+          className=""
           alt=""
         />
       </div>
