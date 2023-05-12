0533b43c87a113ddcee35fcae3a178b2007c6fe9
rithikrajyt <rrpandey855@gmail.com>
2023-05-13 00:50:54 +0530

    done with the achievement heading.Team head left

--- src/components/achievement.js
+++ src/components/achievement.js
@@ -9,8 +9,39 @@ export const Achievement = () => {
         {/* <p className="mb-1 text-xs font-semibold tracking-wide uppercase md:mb-2">
           New History
         </p> */}
+         <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+          <span className="relative inline-block">
+            <svg
+              viewBox="0 0 52 24"
+              fill="currentColor"
+              className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
+            >
+              <defs>
+                <pattern
+                  id="b902cd03-49cc-4166-a0ae-4ca1c31cedba"
+                  x="0"
+                  y="0"
+                  width=".135"
+                  height=".30"
+                >
+                  <circle cx="1" cy="1" r=".7" />
+                </pattern>
+              </defs>
+              <rect
+                fill="url(#b902cd03-49cc-4166-a0ae-4ca1c31cedba)"
+                width="52"
+                height="24"
+              />
+            </svg>
+            <span className="relative">Celebrating </span>
+          </span>{" "}
+          Our Achievements
+        </h2>
         <p className="text-base text-gray-700 md:text-lg">
-          Celebrating Our Achievements
+        
+        </p>
+        <p className="text-base text-gray-700 md:text-lg">
+        Honoring Our Outstanding Achievements
         </p>
       </div>
       <div className="grid gap-6 row-gap-5 lg:grid-cols-3">
