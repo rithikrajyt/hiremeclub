d8bb6b45f6d198aae354af9e29a5ada70d502db5
rithikrajyt <rrpandey855@gmail.com>
2023-05-13 00:33:23 +0530

    Done with the website now

--- src/components/achievement.js
+++ src/components/achievement.js
@@ -1,59 +1,66 @@
+import img1 from "../images/download.png"
+import img2 from "../images/download (2).png"
+import img3 from "../images/123.jpg"
+
 export const Achievement = () => {
-    return (
-      <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-        <div className="max-w-xl mb-6 sm:mx-auto sm:text-center md:mb-10 lg:max-w-2xl">
-          <p className="mb-1 text-xs font-semibold tracking-wide uppercase md:mb-2">
-            New History
-          </p>
-          <p className="text-base text-gray-700 md:text-lg">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-            accusantium doloremque rem aperiam, eaque ipsa quae.
+  return (
+    <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+      <div className="max-w-xl mb-6 sm:mx-auto sm:text-center md:mb-10 lg:max-w-2xl">
+        {/* <p className="mb-1 text-xs font-semibold tracking-wide uppercase md:mb-2">
+          New History
+        </p> */}
+        <p className="text-base text-gray-700 md:text-lg">
+          Celebrating Our Achievements
+        </p>
+      </div>
+      <div className="grid gap-6 row-gap-5 lg:grid-cols-3">
+        <div>
+          <img
+            className="object-cover w-full h-64 mb-6 rounded shadow-lg lg:h-80 xl:h-96"
+            src={img1}
+            alt=""
+          />
+          <h5 className="mb-2 text-xl font-bold leading-none sm:text-2xl">
+            Runner-up At Eureka 2023- IIT Bombay
+          </h5>
+          <p className="text-gray-700">
+            We showcased our expertise and innovation in AI-based shortlisting,
+            emerging as the runner-ups in the Eureka Business Plan Competition
+            and winning a cash prize of 1 lakh rupees.
           </p>
         </div>
-        <div className="grid gap-6 row-gap-5 lg:grid-cols-3">
-          <div>
-            <img
-              className="object-cover w-full h-64 mb-6 rounded shadow-lg lg:h-80 xl:h-96"
-              src="https://images.pexels.com/photos/3184311/pexels-photo-3184311.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;w=500"
-              alt=""
-            />
-            <h5 className="mb-2 text-xl font-bold leading-none sm:text-2xl">
-              A slice of heaven
-            </h5>
-            <p className="text-gray-700">
-              O for awesome, this chocka full cuzzie is as rip-off as a cracker.
-              Meanwhile, in behind the bicycle shed, Hercules Morse.
-            </p>
-          </div>
-          <div>
-            <img
-              className="object-cover w-full h-64 mb-6 rounded shadow-lg lg:h-80 xl:h-96"
-              src="https://images.pexels.com/photos/3184338/pexels-photo-3184338.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-              alt=""
-            />
-            <h5 className="mb-2 text-xl font-bold leading-none sm:text-2xl">
-              Disrupt inspire
-            </h5>
-            <p className="text-gray-700">
-              I'll be sure to note that in my log. Smooth as an android's bottom,
-              eh, Data? When has justice ever been as simple as a rule book?
-            </p>
-          </div>
-          <div>
-            <img
-              className="object-cover w-full h-64 mb-6 rounded shadow-lg lg:h-80 xl:h-96"
-              src="https://images.pexels.com/photos/3184339/pexels-photo-3184339.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
-              alt=""
-            />
-            <h5 className="mb-2 text-xl font-bold leading-none sm:text-2xl">
-              Storage shed
-            </h5>
-            <p className="text-gray-700">
-              Yolo ipsum dolor sit amet, consectetur adipiscing elit. Ut ac
-              suscipit leo. Carpe diem vulputate est nec commodo rutrum.
-            </p>
-          </div>
+        <div>
+          <img
+            className="object-cover w-full h-64 mb-6 rounded shadow-lg lg:h-80 xl:h-96"
+            src={img3}
+            alt=""
+          />
+          <h5 className="mb-2 text-xl font-bold leading-none sm:text-2xl">
+            Finalist at E-Summit 2023- IIT Roorkee
+          </h5>
+          <p className="text-gray-700">
+            We proudly emerged as finalists in the prestigious E-Summit 2023 IIT
+            Roorkee, showcasing our exceptional performance and innovative
+            approach in AI-based shortlisting.
+          </p>
+        </div>
+        <div>
+          <img
+            className="object-cover w-full h-64 mb-6 rounded shadow-lg lg:h-80 xl:h-96"
+            src={img2}
+            alt=""
+          />
+          <h5 className="mb-2 text-xl font-bold leading-none sm:text-2xl">
+            Finalist at Ideathon 4.0
+          </h5>
+          <p className="text-gray-700">
+            Recognized as finalists at the renowned Ideathon 4.0, our
+            exceptional performance and groundbreaking approach in AI-based
+            shortlisting solidified our position among the top contenders in
+            this highly competitive competition.
+          </p>
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
