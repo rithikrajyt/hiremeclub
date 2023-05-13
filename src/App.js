53692e2b93d0316dd2c8ef7686d4abd2ee468e67
rithikrajyt <rrpandey855@gmail.com>
2023-05-13 20:34:38 +0530

    Made changes in Navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Header } from "./components/Header";
 import { Nav } from "./components/Nav";
 import { Feature } from "./components/feature";
@@ -7,6 +8,7 @@ import { Step } from "./components/step";
 import { Team } from "./components/Team";
 import { Achievement } from "./components/achievement";
 import { Footer } from "./components/Footer";
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
@@ -20,9 +22,21 @@ function App() {
       <Achievement />
       <Footer />
     </>
-    
-    
   );
 }
+
+ReactDOM.render(
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={App} />
+      <Route path="/feature" component={Feature} />
+      <Route path="/feature2" component={Feature2} />
+      <Route path="/step" component={Step} />
+      <Route path="/team" component={Team} />
+      <Route path="/achievement" component={Achievement} />
+    </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
 export default App;
